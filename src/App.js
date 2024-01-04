55e97f0e2b908e7498f526d75eb9842e9a523587
One-Liu <lesterhero2002@gmail.com>
2024-01-04 00:17:40 -0600

    user token validation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/registerEmployee" element={<RegisterEmployee />} />
+                    
                 </Routes>
             </BrowserRouter>
         </div>
